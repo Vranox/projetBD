2c2b442b2ea0beb499a280a470365c72957a2c4f
Paul-Elie VALLEE <paul-elie.vallee@epita.fr>
2021-05-25 23:14:00 +0200

    class requeteSQL, simplifie la vie pour faire des appels a la BD

---
+++ Projet BD/src/modele/RequeteSQL.java
@@ -0,0 +1,87 @@
+package modele;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Cette classe permet d'utiliser des fonctions java classiques pour executer des requetes SQL
+ * @author superpaupaul
+ *
+ */
+public class RequeteSQL {
+
+	/**
+	 * Permet d'obtenir le nombre de tuples dans une table donnée
+	 * @param connexion
+	 * @return
+	 */
+	public static int getNumberOfRows(Connection connexion, String table) {
+		int res = 0;
+		try {
+			connexion = ConnexionBD.ConnectFromIUT();
+			Statement stmt = connexion.createStatement();
+			ResultSet rset = stmt.executeQuery("SELECT COUNT(*) FROM " + table);
+			
+			while(rset.next()) {
+				res = rset.getInt("COUNT(*)");
+			}
+			
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	/**
+	 * Permet de renvoyer la liste des livres de la table livre avec une option where ajoutable
+	 * @param connexion
+	 * @return
+	 */
+	public static Livre[] getBooks(Connection connexion,String options) {
+		Livre[] res = new Livre[getNumberOfRows(connexion,"LIVRE")];
+		int index = 0;
+		try {
+			connexion = ConnexionBD.ConnectFromIUT();
+			Statement stmt = connexion.createStatement();
+			ResultSet rset = stmt.executeQuery("SELECT * FROM LIVRE " + options);
+			
+			while(rset.next()) {
+				int id_livre = rset.getInt("ID_LIVRE");
+				String auteur = rset.getString("AUTEUR");
+				String titre = rset.getString("TITRE");
+				
+				res[index] = new Livre(id_livre,auteur,titre);
+				index++;
+			}
+			
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	/**
+	 * Permet d'obtenir une liste de String correspondant aux exemplaires d'un livre
+	 * @param connexion
+	 * @param book
+	 * @return
+	 */
+	public static String[] getId_Exemplaires(Connection connexion, Livre book) {
+		String[] res = new String[getNumberOfRows(connexion,"EXEMPLAIRE")];
+		int index = 0;
+		try {
+			Statement stmt = connexion.createStatement();
+			ResultSet rset = stmt.executeQuery("SELECT ID_EX FROM EXEMPLAIRE WHERE ID_LIVRE = " + book.getId_livre());
+			
+			while(rset.next()) {
+				res[index] = String.valueOf(rset.getInt("ID_EX"));
+				index++;
+			}
+			
+		} catch (SQLException e) {e.printStackTrace();}
+		return res;
+	}
+}
--- Projet BD/src/vue/PanelRecherche.java
+++ Projet BD/src/vue/PanelRecherche.java
@@ -13,19 +13,23 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
 
 import modele.ConnexionBD;
 import modele.Livre;
 import modele.ModeleLivreTable;
+import modele.RequeteSQL;
 /**
  * Ce panel permet aux administrateurs de rechercher des livres, de voir leur disponibilités et de prêter / récupérer un livre
  * @author superpaupaul
@@ -37,69 +41,34 @@ public class PanelRecherche extends JPanel{
 	JButton btnRecherche = new JButton("Go");
 	JTable tableau;
 	String[] intitulesTab = {"ID_LIVRE","AUTEUR","LIVRE"};
-	int lTab;
-	int cTab;
+	JTextArea searchBar = new JTextArea("Rechercher");
+	Livre currentBook;
+	String[] exemplairesLivre;
+	JComboBox comboBox;
+	JLabel titreLivre ;
 	
-	public PanelRecherche() {
+	public PanelRecherche() throws SQLException, IOException {
 		setLayout(new BorderLayout());
-		/*setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		//c.fill = GridBagConstraints.BOTH;
-		c.insets = new Insets(10,10,10,10);
-		add(zoneRecherche,c);
 		
-		c.gridx = 0;
-		c.gridy = 4;
-		add(btnRecherche,c);
-		*/
-		Connection connexion;
+		Connection connexion = ConnexionBD.ConnectFromIUT();
 		tableau = new JTable();
 		ModeleLivreTable tableauModel;
 		Livre[] data;
-		int nbLignes = 0;
 		int counter = 0;
-		JPanel panelGauche = new JPanel();
+		JPanel panelGauche = new JPanel(new BorderLayout());
 		JPanel panelDroite = new JPanel();
 		
-		/**
-		 * CALCUL DE LA TAILLE DU TABLEAU
-		 */
-		try {
-			connexion = ConnexionBD.ConnectFromIUT();
-			Statement stmt = connexion.createStatement();
-			ResultSet rset = stmt.executeQuery("SELECT COUNT(*) FROM LIVRE");
-			
-			while(rset.next()) {
-				nbLignes = rset.getInt("COUNT(*)");
-			}
-			
-		} catch (SQLException | IOException e) {
-			e.printStackTrace();
-		}
 		
-		data = new Livre[nbLignes];
+		
 		
 		/**
-		 * EXECUTION DE LA REQUETE
+		 * Panel Gauche
 		 */
-		try {
-			connexion = ConnexionBD.ConnectFromIUT();
-			Statement stmt = connexion.createStatement();
-			ResultSet rset = stmt.executeQuery("SELECT * FROM LIVRE");
-			
-			while(rset.next()) {
-				int id_livre = rset.getInt("ID_LIVRE");
-				String auteur = rset.getString("AUTEUR");
-				String titre = rset.getString("TITRE");
-				data[counter] = new Livre(id_livre,auteur,titre);
-				counter++;
-			}
-			
-		} catch (SQLException | IOException e) {
-			e.printStackTrace();
-		}
+		
+		
+		data = RequeteSQL.getBooks(connexion,"");
+		
+		
 		
 		// 	PENSER A FAIRE PASSER LA CONNEXION EN PARAMETRE POUR NE PAS AVOIR A FAIRE PLUSIEURS CONNECTIONS
 		
@@ -112,12 +81,39 @@ public class PanelRecherche extends JPanel{
 		JScrollPane scroll = new JScrollPane(tableau,
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		//scroll.setPreferredSize(new Dimension(850,500));
+		//tableau.setDefaultRenderer(Integer.class,new CelluleRenderer());
 
 		tableau.setModel(tableauModel);
-		panelGauche.add(scroll,BorderLayout.WEST);
+		
+		panelGauche.add(searchBar,BorderLayout.NORTH);
+		panelGauche.add(btnRecherche,BorderLayout.NORTH);
+		panelGauche.add(scroll,BorderLayout.SOUTH);
+		
 		add(panelGauche,BorderLayout.WEST);
 		
 		
+		
+		
+		
+		/**
+		 * 	PANEL DROIT
+		 */
+		
+		currentBook = data[0];
+		
+		titreLivre = new JLabel(currentBook.getTitre() + " de " + currentBook.getAuteur() + System.lineSeparator() + "id = " + currentBook.getId_livre());
+		
+		
+		
+		exemplairesLivre = RequeteSQL.getId_Exemplaires(connexion,currentBook);
+		
+		
+		
+		comboBox = new JComboBox(exemplairesLivre);
+		
+		panelDroite.add(titreLivre);
+		panelDroite.add(comboBox);
+		
+		add(panelDroite,BorderLayout.EAST);
 	}
 }
